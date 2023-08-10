a4949dddefb7cf84cff5299cb8477c284b83d632
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-10 11:53:19 -0600

    :lipstick: Implement icons

--- src/todos/TodoItem.js
+++ src/todos/TodoItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AiFillCloseSquare, AiFillCheckSquare } from 'react-icons/ai';
 import './css/TodoItem.css';
 
 function TodoItem(props) {
@@ -6,19 +7,15 @@ function TodoItem(props) {
   const { text, completed } = todo;
   return (
     <li className="TodoItem">
-      <span
+      <AiFillCheckSquare
         className={`CompleteButton flexItem Icon ${completed ? 'CompletedItem' : ''}`}
         onClick={() => onComplete(todo.text)}
-      >
-        V
-      </span>
+      />
       <p className={`flexItem ${completed ? 'TodoItem-p--complete' : ''}`}>{text}</p>
-      <span
+      <AiFillCloseSquare
         className="flexItem DeleteButton Icon"
         onClick={() => onDelete(todo.text)}
-      >
-        X
-      </span>
+      />
     </li>
   );
 }
