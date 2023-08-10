3fa8a096e5c84b51324712675287a80c17ebcdcf
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-10 12:08:54 -0600

    :sparkles: Update messages for counter

--- src/todos/TodoCounter.js
+++ src/todos/TodoCounter.js
@@ -1,6 +1,18 @@
 import './css/TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
+  if (total === 0) {
+    return (
+      <h1>
+        You don't have any TODO 😥
+      </h1>
+    )
+  }
+  if (total === completed) {
+    return (
+      <h1>Congratulations! 🎖 You have complete all your TODOs 🥳</h1>
+    )
+  }
   return (
     <h1>
       You have completed
