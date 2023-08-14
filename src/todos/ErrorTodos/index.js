a919f9a70687b7193e9f761f105bd15d01684658
Nestor Recinos <00083120@UCA.EDU.SV>
2023-08-14 09:55:31 -0600

    :construction: Initialize components

---
+++ src/todos/ErrorTodos/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ErrorTodos.css';
+
+function ErrorTodos() {
+  return (
+    <p>Errors</p>
+  );
+}
+
+export { ErrorTodos };
---
+++ src/todos/LoadingTodos/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoadingTodos.css';
+
+function LoadingTodos() {
+  return (
+    <p>Loading...</p>
+  );
+}
+
+export { LoadingTodos };
